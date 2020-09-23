04648bf6550c13b4051e208e9d83bf2c4a01401f
lpdft <772346730@qq.com>
2020-09-23 18:21:18 +0800

    [ruyue] feat:adjust display style

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,16 +1,36 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Home from './Home'
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Router>
-          <Route exact path="/" component={Home}/>
-          <Route exact path='/about-us' component={About}/>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <Link to="/" className="nav-link" class="nav-link">Home</Link>
+                </li>
+                <li class="nav-item">
+                <Link to="/my-profile" className="nav-link" class="nav-link">My Profile</Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/about-us" className="nav-link" class="nav-link">About Us</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div className="route">
+            <Route exact path='/about-us' component={About}/>
+            <Route exact path='/my-profile' component={Profile}/>
+            <Route exact path="/" component={Home}/>
+          </div>
         </Router>
       </div>
     );
---
+++ src/exercise-1/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Profile extends React.Component {
+  render() {
+    return (<div>
+        <p>Username: XXX</p>
+        <p>Gender: Female</p>
+        <p>Work: IT Industry</p>
+        <p>Website: '/my-profile'</p>
+     </div> 
+    );
+  }
+}
+
+export default Profile;
+
