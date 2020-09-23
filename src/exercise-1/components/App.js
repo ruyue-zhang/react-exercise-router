cce82ccf7dc44f6506ccc4ebb00604f6c029d760
lpdft <772346730@qq.com>
2020-09-23 17:23:28 +0800

    [ruyue] feat:add home page use /

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Router />
+        <Router>
+          <Route exact path="/" component={Home}/>
+        </Router>
       </div>
     );
   }
---
+++ src/exercise-1/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (<div>
+        <p>This is a beautiful Home Page.</p>
+        <p>And the url is '/'.</p>
+     </div> 
+    );
+  }
+}
+
+export default Home;
+
